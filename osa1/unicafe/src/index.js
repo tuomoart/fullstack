5d84b100f3538bab2d726b150faf6af41e64ff2b
Tuomo Artama <tuomo.artama@gmail.com>
2021-01-26 15:55:12 +0200

    1.10 done

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -8,16 +8,24 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+const StatisticsLine = ({text, value}) => {
+  return(
+    <div>
+      {text} {value}
+    </div>
+  )
+}
+
 const Statistics = ({ good, neutral, bad}) => {
   if (good+neutral+bad > 0) {
     return (
       <div>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {good+neutral+bad}</div>
-        <div>average {(good+bad*(-1))/(good+neutral+bad)}</div>
-        <div>positive {good/(good+neutral+bad)*100} %</div>
+        <StatisticsLine text='good' value={good} />
+        <StatisticsLine text='neutral' value={neutral} />
+        <StatisticsLine text='bad' value={bad} />
+        <StatisticsLine text='all' value={good+neutral+bad}/>
+        <StatisticsLine text='average' value={(good+bad*(-1))/(good+neutral+bad)}/>
+        <StatisticsLine text='positive' value={good/(good+neutral+bad)*100 + " %"} />
       </div>
     )
   }
