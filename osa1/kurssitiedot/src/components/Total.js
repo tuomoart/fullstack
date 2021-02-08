567eb2be9416d20d58cc1a9e1069139bdb8f47e8
Tuomo Artama <tuomo.artama@gmail.com>
2021-02-08 20:24:56 +0200

    2.2 ja 2.3 tehty

--- osa1/kurssitiedot/src/components/Total.js
+++ osa1/kurssitiedot/src/components/Total.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+const reducer = (acc, cv) => {
+    console.log(acc)
+    return(acc + cv.exercises)
+}
+
 const Total = (props) => {
     return(
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>Number of exercises {props.parts.reduce(reducer, 0)}</p>
     )
   }
 
