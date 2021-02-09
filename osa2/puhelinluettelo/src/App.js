de02d2e26ab50412ccb81b5966f941c9701726c1
Tuomo Artama <tuomo.artama@gmail.com>
2021-02-09 12:45:11 +0200

    2.9 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -24,12 +24,14 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ search, setSearch ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -59,9 +61,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person => person.name.includes(search))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search for <input
+          value={search}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <h3>Add new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -79,8 +94,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <PersonsDisplay persons={persons} />
+      <h3>Numbers</h3>
+      <PersonsDisplay persons={personsToShow} />
     </div>
   )
 
