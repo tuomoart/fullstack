b1ffe4bc061c4b1d12a8f61481f1cb1c7ad4dd3c
Tuomo Artama <tuomo.artama@gmail.com>
2021-03-04 11:46:44 +0200

    4.2 tehty

--- osa3/blogilista/index.js
+++ osa3/blogilista/index.js
@@ -1,43 +1,11 @@
+const logger = require('./utils/logger')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstack:1qazmko0@cluster0.caikj.mongodb.net/blogilista?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const server = http.createServer(app)
 
 const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(PORT, () => {
+  logger.info(`server running on port ${PORT}`)
 })
