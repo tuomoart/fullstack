64d614122aab4e1a91855e86777ced4bb9ac7492
Tuomo Artama <tuomo.artama@gmail.com>
2021-03-04 15:00:12 +0200

    4.7 asti done

--- osa3/blogilista/utils/list_helper.js
+++ osa3/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -6,7 +8,66 @@ const totalLikes = (blogs) => {
   return(blogs.map((blog) => blog.likes)).reduce(((acc, cur) => acc + cur),0)
 }
 
+const favoriteBlog = (blogs) => {
+  return(
+    blogs.reduce((acc, cur) => {
+      if (cur.likes > acc.likes) {
+        return cur
+      }
+
+      return acc
+    })
+  )
+}
+
+const mostBlogs = (blogs) => {
+  const authors = lodash.countBy(blogs, (blog => blog.author))
+
+  let formattedAuthors = []
+
+  for (const [key, value] of Object.entries(authors)) {
+    formattedAuthors.push({
+      author: key,
+      blogs: value
+    })
+  }
+
+  return formattedAuthors.reduce(((acc, cur) => {
+    if (cur.blogs > acc.blogs) {
+      return cur
+    }
+
+    return acc
+  }))
+}
+
+const mostLikes = (blogs) => {
+  const authors = lodash.groupBy(blogs, (blog => blog.author))
+
+  let formattedAuthors = []
+
+  for (const [key, value] of Object.entries(authors)) {
+    const likes = value.map(blog => blog.likes).reduce((acc, cur) => acc + cur)
+
+    formattedAuthors.push({
+      author: key,
+      likes: likes
+    })
+  }
+
+  return formattedAuthors.reduce(((acc, cur) => {
+    if (cur.likes > acc.likes) {
+      return cur
+    }
+
+    return acc
+  }))
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
