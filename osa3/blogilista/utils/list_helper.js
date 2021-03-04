3e2c67b606b39010ec32921bb8cfac65440a0f59
Tuomo Artama <tuomo.artama@gmail.com>
2021-03-04 12:36:53 +0200

    4.4 asti done

---
+++ osa3/blogilista/utils/list_helper.js
@@ -0,0 +1,12 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return(blogs.map((blog) => blog.likes)).reduce(((acc, cur) => acc + cur),0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
