b6627829575ef6fd671cce3086f5f49cd9ccc211
Tuomo Artama <tuomo.artama@gmail.com>
2021-03-22 14:54:12 +0200

    4.8 done

---
+++ osa3/blogilista/tests/blog_api.test.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    title: 'Blogikirjoitus',
+    author: 'Matti Luukkainen',
+    url: 'www.wikipedia.com',
+    likes: 313
+  },
+  {
+    title: 'Joku toinen blogi',
+    author: 'Tuntematon',
+    url: 'www.blogspot.fi',
+    likes: 0
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+const api = supertest(app)
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct amount of blogs is returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(2)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
