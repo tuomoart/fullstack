c23b4e8a0de99ae4a00bb771b3a36424428f4c8a
Tuomo Artama <tuomo.artama@gmail.com>
2021-03-27 13:10:55 +0200

    4.19 done

--- osa3/blogilista/controllers/blogs.js
+++ osa3/blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+        if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+            return authorization.substring(7)
+        }
+    return null
+    }
 
 blogsRouter.get('/', async (request, response) => {
 
@@ -11,12 +20,17 @@ blogsRouter.get('/', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
 
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
     if (request.body.title === undefined || request.body.url === undefined) {
         response.status(400).end()
         return
     }
-
-    const user = (await User.find({}))[0]
 
     const newBlog = {
         title: request.body.title,
--- osa3/blogilista/utils/errorHandler.js
+++ osa3/blogilista/utils/errorHandler.js
@@ -2,6 +2,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'ValidationError') {
     return response.status(400).send({ error: 'invalid username'})
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
   }
 }
 
