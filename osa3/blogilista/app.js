250da79083a1d5ba76543d2bf888f5a54eb14900
Tuomo Artama <tuomo.artama@gmail.com>
2021-03-27 13:26:09 +0200

    4.20 done

--- osa3/blogilista/app.js
+++ osa3/blogilista/app.js
@@ -8,11 +8,13 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const errorHandler = require('./utils/errorHandler')
+const tokenExtractor = require('./utils/tokenExtractor')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
+app.use(tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
--- osa3/blogilista/controllers/blogs.js
+++ osa3/blogilista/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-        if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-            return authorization.substring(7)
-        }
-    return null
-    }
-
 blogsRouter.get('/', async (request, response) => {
 
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1})
@@ -20,7 +12,7 @@ blogsRouter.get('/', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
 
-    const token = getTokenFrom(request)
+    const token = request.token
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if (!token || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
