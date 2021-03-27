2eb2173ddab86daa5125c9077dc7ad810bf8818e
Tuomo Artama <tuomo.artama@gmail.com>
2021-03-27 12:53:40 +0200

    4.18 done

--- osa3/blogilista/app.js
+++ osa3/blogilista/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const errorHandler = require('./utils/errorHandler')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -14,6 +15,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(errorHandler)
 
